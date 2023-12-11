8477f57656bfb9cbfb66d8a914224f5f64b3986b
emoee <emoee@Office.uhs.ac.kr>
2023-12-11 12:23:49 +0900

    ✨ 4일차 - 객체 개념 정리 및 클래스 다이어그램 작성

---
+++ JAVA/JavaGame/src/mycom/dept/game/Play.java
@@ -0,0 +1,9 @@
+package mycom.dept.game;
+
+public class Play {
+	public static void main(String[] args) {
+		//player1
+		Player player1 = new Player();
+		//enemy1,2,3,4
+	}
+}
---
+++ JAVA/JavaGame/src/mycom/dept/game/Player.java
@@ -0,0 +1,18 @@
+package mycom.dept.game;
+
+public class Player {
+	private int x;
+	private int y;
+	private String shape;
+	
+	public void display() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	public void moveRight() {
+		
+	}
+}
