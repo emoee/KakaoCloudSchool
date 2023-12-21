39905afbaa90888644df60a7aae7ca2ef36547a4
emoee <emoee@Office.uhs.ac.kr>
2023-12-21 11:26:08 +0900

    ✨ 입출력: 입력 스트림 예제

---
+++ JAVA/JavaIO/src/mycom/dept/test/Main.java
@@ -0,0 +1,24 @@
+package mycom.dept.test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		FileInputStream fileInputStream=null;
+		try {
+			fileInputStream=new FileInputStream("Hello.txt");
+			int ch=0;
+			while((ch = fileInputStream.read()) != -1) {
+				System.out.println(ch);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 없습니다.");
+		} catch (IOException e) {   
+			System.out.println("파일이 끝났습니다.");
+		} finally {
+			if (fileInputStream != null) fileInputStream.close();
+		}
+	}
+}
