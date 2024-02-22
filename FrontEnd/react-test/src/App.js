9b7e54af252512dc99899df3f79ecfead0e10d9b
minsun <elinnane0@gmail.com>
2024-02-22 12:04:41 +0900

    ✨ React useState 사용

--- FrontEnd/react-test/src/App.js
+++ FrontEnd/react-test/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Counter from './Counter';
 import CounterState from './CounterState';
 import StateSample from './StateSample';
+import Say from './Say';
 
 function App() {
   return (
     <div className="App">
       <h1>🎱 React Test 🎱</h1>
+      <Say/>
       <CounterState/>
       <StateSample/>
       <Counter superName="쿠크다스" superType="과자">내용</Counter>
